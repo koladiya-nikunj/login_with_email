5d059ca434c35f487d777e76cfe7896d2f5f0e23
koladiya-nikunj <koladiyanikunj124@gmail.com>
2024-03-11 10:28:16 +0530

    done

---
+++ src/routes/userRoute.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const router = express();
+const passport = require('passport'); 
+require('../passport/passport');
+
+router.use(passport.initialize()); 
+router.use(passport.session());
+
+const userController = require('../controllers/userController');
+
+router.get('/', userController.loadAuth);
+
+// Auth 
+router.get('/auth/google', passport.authenticate('google', {
+    scope: ['email', 'profile', 'https://www.googleapis.com/auth/userinfo.email'],
+    prompt: 'select_account'
+}));
+
+// Auth Callback 
+router.get( '/auth/google/callback', 
+	passport.authenticate( 'google', { 
+		successRedirect: '/success', 
+		failureRedirect: '/failure' 
+}));
+
+// Success 
+router.get('/success' , userController.successGoogleLogin); 
+
+// failure 
+router.get('/failure' , userController.failureGoogleLogin);
+
+module.exports = router;
